123752129767d4d951f42c4ad3cf6aee74bc2701
Akash <akashkurdekar@gmail.com>
2014-12-19 23:35:46 +0000

    refactored to use more symbols

--- roman-numerals.js
+++ roman-numerals.js
@@ -1,26 +1,30 @@
-var ROMAN_SYMBOLS = {
-  'I': 1,
-  'V': 5,
-  'X': 10,
-  'L': 50,
-  'C': 100,
-  'D': 500,
-  'M': 1000
-};
+var ROMAN_SYMBOLS = [
+  {symbol: 'IV', value: 4},
+  {symbol: 'IX', value: 9},
+  {symbol: 'XL', value: 40},
+  {symbol: 'XC', value: 90},
+  {symbol: 'CD', value: 400},
+  {symbol: 'CM', value: 900},
+
+  {symbol: 'I', value: 1},
+  {symbol: 'V', value: 5},
+  {symbol: 'X', value: 10},
+  {symbol: 'L', value: 50},
+  {symbol: 'C', value: 100},
+  {symbol: 'D', value: 500},
+  {symbol: 'M', value: 1000}
+];
 
 exports.fromRoman = function (roman) {
-  var symbols = roman.split('');
   var total = 0;
 
-  for (var i = 0; i < symbols.length; i++) {
-    var symbolValue = ROMAN_SYMBOLS[symbols[i]];
-    var prevValue = i === 0 ? null : ROMAN_SYMBOLS[symbols[i - 1]];
-
-    if (prevValue && prevValue < symbolValue) {
-      total -= 2 * prevValue;
-    }
-      total += symbolValue;
-  }
+  ROMAN_SYMBOLS.forEach(function (symbol) {
+    var regex = new RegExp(symbol.symbol, 'g');
+    var matches = roman.match(regex) || [];
+    var count = matches.length;
+    total += count * symbol.value;
+    roman = roman.replace(regex, '');
+  });
   return total;
 };
 
