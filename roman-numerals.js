d2341a8a2353b3a77a16f949ada898548823e390
Akash <akashkurdekar@gmail.com>
2014-12-19 21:28:19 +0000

    symbols

--- roman-numerals.js
+++ roman-numerals.js
@@ -1,5 +1,15 @@
-exports.fromRoman = function () {
+var ROMAN_SYMBOLS = {
+  'I': 1,
+  'V': 5,
+  'X': 10,
+  'L': 50,
+  'C': 100,
+  'D': 500,
+  'M': 1000
+};
 
+exports.fromRoman = function (roman) {
+  return ROMAN_SYMBOLS[roman];
 };
 
 exports.toRoman = function () {
--- roman-numerals.spec.js
+++ roman-numerals.spec.js
@@ -4,12 +4,40 @@ describe('RomanNumerals', function () {
   it('is an object', function () {
     expect(typeof RomanNumerals).toBe('object');
   });
-  
-  it('has a function property toRoman', function () {
-    expect(typeof RomanNumerals.toRoman).toBe('function');
-  });
 
-  it('has a function property fromRoman', function () {
-    expect(typeof RomanNumerals.fromRoman).toBe('function');
+  describe('fromRoman', function () {
+    it('is a function property of RomanNumerals', function () {
+      expect(typeof RomanNumerals.fromRoman).toBe('function');
+    });
+
+    describe('Symbols', function () {
+      it('converts I to 1', function () {
+        expect(RomanNumerals.fromRoman('I')).toBe(1);
+      });
+
+      it('converts V to 5', function () {
+        expect(RomanNumerals.fromRoman('V')).toBe(5);
+      });
+
+      it('converts X to 10', function () {
+        expect(RomanNumerals.fromRoman('X')).toBe(10);
+      });
+
+      it('converts L to 50', function () {
+        expect(RomanNumerals.fromRoman('L')).toBe(50);
+      });
+
+      it('converts C to 100', function () {
+        expect(RomanNumerals.fromRoman('C')).toBe(100);
+      });
+
+      it('converts D to 500', function () {
+        expect(RomanNumerals.fromRoman('D')).toBe(500);
+      });
+
+      it('converts M to 500', function () {
+        expect(RomanNumerals.fromRoman('M')).toBe(1000);
+      });
+    });
   });
 });
